e8431bc270fcd1de8800a9dacf405e5247ba18ce
Murray Cadzow <murray.cadzow@otago.ac.nz>
2020-07-28 13:52:32 +1200

    Rmarkdown examples used for digi-syska creating google scholar reports

---
+++ scholar/create_reports.R
@@ -0,0 +1,27 @@
+# can use a shiny interface to interactively fill in the parameters:
+knit_with_parameters('~/Git_repos/Digi_SYKSA/example.Rmd')
+
+
+# or prgrammatically create reports by iterating over supplied values
+ids <- c("B7vSqZsAAAAJ", "amYIKXQAAAAJ&hl")
+
+create_report <- function(id){
+  # does create awful file names in this instance because scholar ids are ugly
+  # but this function could be altered to make them much nicer
+
+  rmarkdown::render("scholar/example_parameter_template.Rmd", # name of markdown document to use
+                    params = list(id = id), # values for the parameter
+                    output_file = paste0("report_", id,".html")) # output file name of compiled doc
+}
+
+
+purrr::map(ids, create_report)
+
+
+# same task but using a for loop instead
+# and without creating custom function to make it nicer
+for(person in ids){
+  rmarkdown::render("scholar/example_parameter_template.Rmd",
+                    params = list(id = person),
+                    output_file = paste0("report_", person,".html"))
+}
---
+++ scholar/intro_to_r.R
@@ -0,0 +1,13 @@
+# defining my name
+my_name <- "Murray"
+
+
+# excel formula
+# =SUM(1, 4, 6)
+
+# equivalent in R:
+sum(1, 4, 6)
+
+install.packages("tidyverse")
+install.packages("scholar")
+
